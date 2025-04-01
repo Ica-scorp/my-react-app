13b9b6b3c2698b3fbb72f69370bf079d1d541dcc
Ica-scorp <c.amy@wustl.com>
2025-04-01 16:29:51 -0500

    changed button square style

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,43 @@
-export default function Square() {
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
   return (
     <>
-      <button className="square">X</button>
-      <button className="square">X</button>
+      <h1>Hi</h1>
+      <Board></Board>
+    </>
+  )
+}
+
+export default App
+
+function Square({value}){
+  return <button className="square">{value}</button>;
+}
+
+function Board() {
+  return (
+    <>
+      <div className="board-row">
+        <button className="square">1</button>
+        <button className="square">2</button>
+        <button className="square">3</button>
+      </div>
+      <div className="board-row">
+        <button className="square">4</button>
+        <button className="square">5</button>
+        <button className="square">6</button>
+      </div>
+      <div className="board-row">
+        <button className="square">7</button>
+        <button className="square">8</button>
+        <button className="square">9</button>
+      </div>
     </>
   );
 }

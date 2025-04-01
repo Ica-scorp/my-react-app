cf4135ed0bff045a9508148f21740d0f1e7ada5e
Ica-scorp <c.amy@wustl.com>
2025-04-01 16:41:08 -0500

    removed the value passed to square in board

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
 
 export default App
 
-function Square({value}){
+function Square(){
   const [value, setValue] = useState(null);
   //define the onclick function
   function handleClick() {
@@ -36,19 +36,19 @@ function Board() {
   return (
     <>
       <div className="board-row">
-        <Square value="1"/>
-        <Square value="2"/>
-        <Square value="3"/>
+        <Square/>
+        <Square/>
+        <Square/>
       </div>
       <div className="board-row">
-        <Square value="4"/>
-        <Square value="5"/>
-        <Square value="6"/>
+        <Square/>
+        <Square/>
+        <Square/>
       </div>
       <div className="board-row">
-        <Square value="7"/>
-        <Square value="8"/>
-        <Square value="9"/>
+        <Square/>
+        <Square/>
+        <Square/>
       </div>
     </>
   );

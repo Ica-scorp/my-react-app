e843d51ab2fd8669f17e6203796a2f88d6e104d3
Ica-scorp <c.amy@wustl.edu>
2025-04-03 16:36:27 -0500

    save and change states in game not board

--- src/App.jsx
+++ src/App.jsx
@@ -14,9 +14,7 @@ function Square({value,onSquareClick}){
   );
 }
 
-function Board() {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));//initialize the board
+function Board({xIsNext, squares, onPlay}) {
   function handleClick(i) {
     if (squares[i] || calculateWinner(squares)) {
       return;
@@ -27,8 +25,7 @@ function Board() {
     } else {
       nextSquares[i] = "O";
     }
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares);
   }
   const winner = calculateWinner(squares);
   let status;
@@ -86,10 +83,11 @@ export default function Game() {
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const currentSquares = history[history.length - 1];
   function handlePlay(nextSquares) {
-    // TODO
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
   }
 
-  
+
   return (
     <div className="game">
       <div className="game-board">

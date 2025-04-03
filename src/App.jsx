534e01bfb057c05c649e6f652a5c1a88422997fc
Ica-scorp <c.amy@wustl.edu>
2025-04-03 16:15:22 -0500

    added a generic onclick function inside board component

--- src/App.jsx
+++ src/App.jsx
@@ -16,9 +16,10 @@ function App() {
 
 export default App
 
-function Square({value}){
+function Square({value,onSquareClick}){
   return (
     <button 
+      onClick={onSquareClick}
       className="square"
     >
       {value}
@@ -28,22 +29,27 @@ function Square({value}){
 
 function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));//initialize the board
+  function handleClick() {
+    const nextSquares = squares.slice();
+    nextSquares[0] = "X";
+    setSquares(nextSquares);
+  }
   return (
     <>
       <div className="board-row">
-        <Square value={squares[0]}/>
-        <Square value={squares[1]}/>
-        <Square value={squares[2]}/>
+        <Square value={squares[0]} onSquareClick={handleClick}/>
+        <Square value={squares[1]} onSquareClick={handleClick}/>
+        <Square value={squares[2]} onSquareClick={handleClick}/>
       </div>
       <div className="board-row">
-        <Square value={squares[3]}/>
-        <Square value={squares[4]}/>
-        <Square value={squares[5]}/>
+        <Square value={squares[3]} onSquareClick={handleClick}/>
+        <Square value={squares[4]} onSquareClick={handleClick}/>
+        <Square value={squares[5]} onSquareClick={handleClick}/>
       </div>
       <div className="board-row">
-        <Square value={squares[6]}/>
-        <Square value={squares[7]}/>
-        <Square value={squares[8]}/>
+        <Square value={squares[6]} onSquareClick={handleClick}/>
+        <Square value={squares[7]} onSquareClick={handleClick}/>
+        <Square value={squares[8]} onSquareClick={handleClick}/>
       </div>
     </>
   );
